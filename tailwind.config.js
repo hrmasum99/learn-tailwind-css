87da07135d5c5038ac1aec4ef2a5086a41bca283
Habibur Rahman Masum <hrmasum99.study@gmail.com>
2025-09-17 21:54:18 +0600

    epixelab init

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         // This makes Poppins the default sans-serif font
+//         sans: ['Poppins', 'sans-serif'],
+        
+//         // You can still keep your other fonts available as well
+//         abel: ['Abel', 'sans-serif'],
+//         'dm-sans': ['"DM Sans"', 'sans-serif'],
+//         roboto: ['Roboto', 'sans-serif'],
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
